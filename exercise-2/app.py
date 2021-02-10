46cbbc7e1c85815665658fcbb597cc7ba9ba4878
Chudier <chudierp@gmail.com>
2021-02-09 19:36:49 -0700

    exercise two complete

---
+++ exercise-2/app.py
@@ -0,0 +1,65 @@
+import os
+import requests
+
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, send_file
+
+
+################################################
+## SETUP
+################################################
+
+app = Flask(__name__)
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+
+################################################
+## ROUTES
+################################################
+
+@app.route('/')
+def home():
+    """Displays the homepage with forms for current or historical data."""
+    context = {
+        'min_date': (datetime.now() - timedelta(days=5)),
+        'max_date': datetime.now()
+    }
+    return render_template('home.html', **context)
+
+def get_letter_for_units(units):
+    """Returns a shorthand letter for the given units."""
+    return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
+@app.route('/results')
+def results():
+    """Displays results for current weather conditions."""
+    city = request.args.get('city')
+    units = request.args.get('requested_units')
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units={units}'
+    # params = {
+    #     'appid': API_KEY,
+    #     'place': city,
+    #     'units': units
+    # }
+    result_json = requests.get(url).json()
+    print('JSON DATA', result_json)
+
+    context = {
+        'date': datetime.now(),
+        'city': result_json['name'],
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'units_letter': get_letter_for_units(units)
+    }
+
+    return render_template('results.html', **context)
+
+
+if __name__ == '__main__':
+    app.run()
